29e6e28aa23217c3fdb727b3ab783fb3bb316bb3
Aswathy Din Nair <aswathydin@gmail.com>
2026-03-10 23:14:08 +0530

    message

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Input text");
-        String Str=sc.nextLine();
-        StringBuilder sb  = new StringBuilder();
-        for(int i=Str.length()-1;i>=0;i--){
-            sb.append(Str.charAt(i));
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+
+        System.out.println("Input text:");
+        String str = sc.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
         }
-       String reverse=sb.toString();
-        if(Str.equals(reverse)){
-            System.out.println("is it Palindrome? true");
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
         }
-        else{
-            System.out.println("Is it Palindrome? False");
+        if (str.equals(reversed)) {
+            System.out.println("Is it Palindrome? true");
+        } else {
+            System.out.println("Is it Palindrome? false");
         }
+        sc.close();
     }
 }
