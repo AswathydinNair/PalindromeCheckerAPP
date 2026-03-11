4011a8357b9278845bdc7bd4e46f177a3b20e59a
Aswathy Din Nair <aswathydin@gmail.com>
2026-03-11 12:20:06 +0530

    message

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,75 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Input text:");
         String str = sc.nextLine();
-        char[] chars = str.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
 
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < str.length(); i++) {
+            Node newNode = new Node(str.charAt(i));
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
 
-        System.out.println("Is it Palindrome? " + isPalindrome);
+        if (isPalindrome) {
+            System.out.println("Is it Palindrome? true");
+        } else {
+            System.out.println("Is it Palindrome? false");
+        }
+
+        sc.close();
     }
 }
